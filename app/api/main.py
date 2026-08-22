06bb6c5c8bf134e4190a7172b29e5270de148c96
elbryan123blxd-ai <bryan@cloudops.local>
2026-08-22 12:15:38 -0500

    test: disparar workflow

--- app/api/main.py
+++ app/api/main.py
@@ -13,7 +13,7 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-    await engine.dispose()
+    await engine.dispose()          ##hola
 
 
 app = FastAPI(
